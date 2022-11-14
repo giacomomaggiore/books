a23ceb08cb66bc53e93764f50994d72171e283c9
giacomomaggiore <giaco.maggiore@gmail.com>
2022-11-14 21:44:33 +0100

    added carchitecture

--- public/js/database-Jackson.js
+++ public/js/database-Jackson.js
@@ -255,12 +255,7 @@ const libri = [
         src: "https://www.ibs.it/images/9788822707475_0_536_0_75.jpg",
         link: "https://www.amazon.it/scorretto-efficace-liberarsi-irritanti-problemi/dp/8822707451/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=&sr=",
     },
-    {
-        titolo: "CARCHITECTURE - Houses with horsepower",
-        citazione: "Homes should be built in factories similar to those for automobiles",
-        src: "https://www.ibs.it/images/9788822707475_0_536_0_75.jpg",
-        link: "https://www.amazon.it/scorretto-efficace-liberarsi-irritanti-problemi/dp/8822707451/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=&sr=",
-    },
+
 
 
 
